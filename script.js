cbb5112539d524827a07fa00fca55f546621b2b2
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-03 20:45:18 +0200

    add style and js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const burger = document.querySelector('.burger');
-const iconBurger = document.querySelector('.fa-bars');
-const iconX = document.querySelector('.fa-times');
-const column = document.querySelector('aside');
+const activeElement = document.querySelectorAll('.active')
 
 burger.addEventListener('click', function() {
-	iconBurger.classList.toggle('show');
-	iconX.classList.toggle('show');
-	column.classList.toggle('show');
+	for(let i = 0; i < activeElement.length; i++) {
+		activeElement[i].classList.toggle('show')
+	}
 })
